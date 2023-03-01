772faabbed6f74f6989f72822dff360fed15fb24
Wilfried <122891191+Jukiyoomi@users.noreply.github.com>
2023-03-01 15:00:32 +0100

    composant header

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import Header from "./components/Header.jsx";
+
 function App() {
 
 	return (
-		<div className="App">
-			Hello World
-		</div>
+		<>
+			<Header />
+		</>
 	)
 }
 
---
+++ src/components/Header.jsx
@@ -0,0 +1,7 @@
+export default function Header() {
+	return (
+		<header className="w-full p-4 bg-white flex items-center">
+			<ion-icon name="menu-outline"></ion-icon>
+		</header>
+	)
+}
